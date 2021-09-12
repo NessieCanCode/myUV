442c5d4a304bea78fc5e8d88adcd76d35f49dd5e
Robert Romero <1013414+NessieCanCode@users.noreply.github.com>
2021-09-11 19:37:32 -0700

    Update lambda_function.py
    fixed a logic issue.

--- myUV_Daily_Forecast_LaMetric/lambda_function.py
+++ myUV_Daily_Forecast_LaMetric/lambda_function.py
@@ -16,16 +16,16 @@ def lambda_handler(event, context):
     data = response.json()
     uv_index = data[0]['UV_INDEX']
     uv_alert = data[0]['UV_ALERT']
-    if uv_index in range(0, 2):
+    if uv_index in range(0, 3):
         uv_icon = 37949
         uv_recommend="You can safely enjoy being outside. Wear sunglasses on bright days. If you burn easily, cover up and use sunscreen SPF 30+. In winter, reflection off snow can nearly double UV strength."
-    elif uv_index in range(3, 5):
+    elif uv_index in range(2, 6):
         uv_icon = 37950
         uv_recommend="Take precautions if you will be outside, such as wearing a hat and sunglasses and using sunscreen SPF 30+. Reduce your exposure to the sun's most intense UV radiation by seeking shade during midday hours."
-    elif uv_index in range(6, 7):
+    elif uv_index in range(5, 8):
         uv_icon = 37951
         uv_recommend="Protection against sun damage is needed. Wear a wide-brimmed hat and sunglasses, use sunscreen SPF 30+ and wear a long-sleeved shirt and pants when practical. Reduce your exposure to the sun's most intense UV radiation by seeking shade during midday hours."
-    elif uv_index in range(8, 10):
+    elif uv_index in range(7, 11):
         uv_icon = 37952
         uv_recommend="Protection against sun damage is needed. If you need to be outside during midday hours between 10 a.m. and 4 p.m., take steps to reduce sun exposure. A shirt, hat and sunscreen are a must, and be sure you seek shade. Beachgoers should know that white sand and other bright surfaces reflect UV and can double UV exposure."
     elif uv_index >= 11:
